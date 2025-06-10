1ab12a1a94d0093a52f14e272816cee0f18a0b16
ganteng <dexoexa@gmail.com>
2025-06-10 21:23:48 +0700

    first commit

---
+++ src/com/lat1/ifelse.java
@@ -0,0 +1,17 @@
+package com.lat1;
+public  class if {
+
+  Main m = new Main();
+  public void ifelse() {
+    int a = 10;
+    int b = 20;
+    if (a > b) {
+      m.print("a is greater than b");
+    } else if (a < b) {
+      m.print("a is less than b");
+    } else {
+      m.print("a is equal to b");
+    }
+  }//cetak
+  
+}
